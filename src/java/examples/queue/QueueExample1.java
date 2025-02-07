aceafe450a35686f719c3863318a5425bbcb46f4
Marcus Alexander Dahl <marcus@programkode.studio>
2025-02-07 15:18:25 +0100

    Update queue code example to use record instead of static class

--- src/java/examples/queue/QueueExample1.java
+++ src/java/examples/queue/QueueExample1.java
@@ -1,17 +1,7 @@
 import data.structures.queue.SinglyLinkedListLinearQueue;
 
 
-class Person {
-    private String name;
-
-    public Person(String name) {
-        this.name = name;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-}
+record Person(String name) {}
 
 
 void main() {
@@ -21,15 +11,15 @@ void main() {
     queue.enqueue(new Person("Bob"));
     queue.enqueue(new Person("Charlie"));
 
-    System.out.printf("Next person in the queue: %s%n", queue.peek().getName());
+    System.out.printf("Next person in the queue: %s%n", queue.peek().name());
 
     var nextPerson = queue.dequeue();
 
-    if (nextPerson.getName().equals("Alice")) {
+    if (nextPerson.name().equals("Alice")) {
         System.out.println("Alice entered the queue first and was removed from the queue first!");
     }
 
-    if (queue.peek().getName().equals("Bob")) {
+    if (queue.peek().name().equals("Bob")) {
         System.out.println("Bob is next in the queue after Alice was removed from the queue");
     }
 }
