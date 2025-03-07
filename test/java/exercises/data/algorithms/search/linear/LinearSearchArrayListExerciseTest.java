b9851d8b04836f5b1097298e70e703c24d9716b7
Anders Stensland <43165915+andersstensland@users.noreply.github.com>
2025-03-07 15:17:19 +0100

    FEAT: Finsihed testLinearSearch method

--- test/java/exercises/data/algorithms/search/linear/LinearSearchArrayListExerciseTest.java
+++ test/java/exercises/data/algorithms/search/linear/LinearSearchArrayListExerciseTest.java
@@ -11,8 +11,9 @@ import org.junit.jupiter.api.TestMethodOrder;
 import org.junitpioneer.jupiter.DisableIfTestFails;
 
 import java.lang.reflect.Method;
-import java.util.Arrays;
+import java.util.ArrayList;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
 
 
@@ -30,15 +31,32 @@ class LinearSearchArrayListExerciseTest
         super("Test code that performs a linear search on an ArrayList data structure");
     }
 
+    //# Test Fields
+    private ArrayList<Integer> list1;
+    private ArrayList<String> list2;
 
     //# Setup
     @BeforeEach
     void setUp() {
+        // Initializing the lists before each test
+        list1 = new ArrayList<>();
+        list1.add(1);
+        list1.add(2);
+        list1.add(3);
+        list1.add(4);
+        list1.add(5);
 
+        list2 = new ArrayList<>();
+        list2.add("apple");
+        list2.add("banana");
+        list2.add("cherry");
     }
 
     @AfterEach
     void tearDown() {
+        // Clean up resources
+        list1.clear();
+        list2.clear();
     }
 
 
@@ -61,6 +79,7 @@ class LinearSearchArrayListExerciseTest
     private Class<?> getTestClass() throws ClassNotFoundException {
         return Class.forName("%s.%s".formatted(pkg, identifier));
     }
+
 
     /** Given a class abstraction for instantiation,
      *  i.e. java.lang.Class, run main method as entry point.
@@ -94,8 +113,6 @@ class LinearSearchArrayListExerciseTest
         try {
             Class<?> cls = getTestClass();
 
-
-
         } catch (ClassNotFoundException e) {
             fail("Class named \"%s\" is not located at: %s%n".formatted(identifier, "java.%s".formatted(pkg)));
         }
@@ -103,11 +120,32 @@ class LinearSearchArrayListExerciseTest
         System.out.println("task1");
     }
 
-
     @Test
     @Order(2)
     @DisplayName("Task 2: Create static method")
     void createStaticMethod() {
         System.out.println("task2");
+    }
+
+    @Test
+    @Order(3)
+    @DisplayName("Task 3: Create a linear search method")
+    void testLinearSearch() throws Exception {
+        Class<?> cls = getTestClass();
+        Method linearSearchMethod = cls.getMethod("linearSearch", ArrayList.class, Object.class);
+
+        // Test searching for an integer in list1
+        int result1 = (int) linearSearchMethod.invoke(null, list1, 4);
+        assertEquals(3, result1); // 4 is at index 3
+
+        int result2 = (int) linearSearchMethod.invoke(null, list1, 6);
+        assertEquals(-1, result2); // 6 not found
+
+        // Test searching for a string in list2
+        int result3 = (int) linearSearchMethod.invoke(null, list2, "banana");
+        assertEquals(1, result3); // "banana" at index 1
+
+        int result4 = (int) linearSearchMethod.invoke(null, list2, "pear");
+        assertEquals(-1, result4); // "pear" not found
     }
 }
